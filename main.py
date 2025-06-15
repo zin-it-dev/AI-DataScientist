9699fd36f219e70ebd39e09c22f7bfb23d0b0383
Lê Hiển Vinh <zin.it.dev@gmail.com>
2025-06-15 11:04:16 +0700

    🚀 Data Visualization (Part 5)
    🦖 Distributions

--- main.py
+++ main.py
@@ -2,14 +2,22 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv('./data/insurance.csv')
+df = pd.read_csv('./data/iris.csv', index_col='Id')
 
 print(df.head())
 
-plt.figure(figsize=(10, 6))
+plt.figure(figsize=(5, 4))
 
-sns.scatterplot(x=df['bmi'], y=df['charges'], hue=df['smoker'])
+# sns.histplot(data=df, x='Petal Length (cm)', hue='Species')
 
-# sns.regplot(x=df['bmi'], y=df['charges'])
+# plt.title("Histogram of Petal Lengths, by Species")
+
+# sns.kdeplot(data=df['Petal Length (cm)'], fill=True)
+
+# sns.jointplot(x=df['Petal Length (cm)'], y=df['Sepal Width (cm)'], kind='kde')
+
+sns.kdeplot(data=df, x='Petal Length (cm)', hue='Species', fill=True)
+
+plt.title("Distribution of Petal Lengths, by Species")
 
 plt.show()
